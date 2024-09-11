2e507e2b3638a6e5c559d30a4c8dac5425ea3f3a
Ali Akbari <alimustanserakbari@gmail.com>
2024-09-10 20:22:41 -0600

    First Commit For Class CPSC 449 Programming Paradigms

---
+++ In_Class/HelloWorld.hs
@@ -0,0 +1,12 @@
+import qualified Distribution.InstalledPackageInfo as HelloWorld
+
+main :: IO ()
+main = putStrLn "Hello World"
+
+{- 
+HOW TO RUN (make sure to save)
+ghci
+:cd In_Class
+:load HelloWorld.hs
+main
+-}
