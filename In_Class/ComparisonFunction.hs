88353672a5ac8f6f1258523b96b1995793da7d4d
Ali Akbari <alimustanserakbari@gmail.com>
2024-09-11 16:02:38 -0600

    Fixed ComparisonFunction

--- In_Class/ComparisonFunction.hs
+++ In_Class/ComparisonFunction.hs
@@ -1,7 +1,7 @@
 
--- Comparison Function
-ComparisonFunction :: Int -> Int
-ComparisonFunction x 
+-- Comparison Function 
+comparisonFunction :: Int -> Int
+comparisonFunction x 
     | x < 0      = -1
     | x == 0     = 0
     | otherwise  = 1
